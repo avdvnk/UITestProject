60b57ee8a7356153921e882e1c0b014c055c7fbe
avdvnk <a.vdvnk@mail.ru>
2022-12-07 17:17:44 +0300

    Documentation fixed

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -10,7 +10,7 @@ import static com.codeborne.selenide.Selectors.byTagAndText;
 import static com.codeborne.selenide.Selenide.$;
 
 /**
- * Base Selenide page with common methods and fields
+ * Base Selenide page with common methods and locators
  */
 public class BasePage {
 
